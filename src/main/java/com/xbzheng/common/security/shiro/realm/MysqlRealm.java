9fa5c479647960f98fa8e4a2d113601122cd020c
zhenghun <578365424@qq.com>
2015-07-20 22:54:46 +0800

    mmmm

--- src/main/java/com/xbzheng/common/security/shiro/realm/MysqlRealm.java
+++ src/main/java/com/xbzheng/common/security/shiro/realm/MysqlRealm.java
@@ -1,11 +1,14 @@
 package com.xbzheng.common.security.shiro.realm;
 
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.Subject;
 
 /**
  * Created by Administrator on 2015/7/20.
@@ -23,12 +26,15 @@ public class MysqlRealm  extends AuthorizingRealm{
 
     /**
      * 进行认证
-     * @param token
+     * @param authcToken
      * @return
      * @throws AuthenticationException
      */
     @Override
-    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authcToken) throws AuthenticationException {
+        UsernamePasswordToken token = (UsernamePasswordToken)authcToken;
+        Subject subject = SecurityUtils.getSubject();
+        subject.login(token);
         return null;
     }
 }
